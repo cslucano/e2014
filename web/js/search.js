3d7e2f7ea8e0e6dff2f301c66766b4d035e1ca8f
Carlos Salvatierra <cslucano@gmail.com>
2014-08-31 16:32:39 -0500

    adding facet cookie interaction

--- web/js/search.js
+++ web/js/search.js
@@ -4,9 +4,10 @@ SMID = 'data-searchmod';
 // stored search modifier cookie name
 SMCOOKIE = 'search-modifier-cookie';
 
-// get faceted/slug data if the user is on a special page or explicitly 
+// get faceted/slug data if the user is on a special page or explicitly
 // using search facets
 function getFacets() {
+    console.log('getFacets: begin');
     var facets = [];
     var rawslug = $('#slugdata').text();
     var lmark = "(u'selected_facets', ";
@@ -22,21 +23,26 @@ function getFacets() {
 	trimmed = trimmed.split("'").join('"');
 	var facets = JSON.parse(trimmed);
     }
-    
+
     return facets;
 }
 
 function checkSearch() {
-    var node = $('#id_q');
-    var searchTerm = node.val();
+    console.log('checkSearch: begin');
+    var node_query = $('#bq_query');
+    var node_location = $('#bq_location');
+    var searchTerm = node_query.val();
+    var locationTerm = node_location.val();
 
     var facets = getFacets();
 
-    if ((searchTerm.trim() == "") && !facets.length) {
-        node.focus();
-        node.css("border-color", "#ff0039");
+    if (
+        (searchTerm.trim() == "" && locationTerm.trim() == "") && !facets.length
+        ) {
+        node_query.focus();
+        node_query.css("border-color", "#ff0039");
         $('#term-required').show();
-        
+
         // stop form submission
         return false;
     }
@@ -44,45 +50,45 @@ function checkSearch() {
 
         try {
             //console.log('checkSearch: before GA');
-            _gaq.push(['_trackEvent', 'homepage' , 'search']);
+            //_gaq.push(['_trackEvent', 'homepage' , 'search']);
             //console.log('checkSearch: after GA');
-        } 
+        }
+        catch (err) {
 
-	catch (err) { 
-	}
+        }
 
-	var modifiers = getSearchModifiers();
-	var ordering = getSortModifier();
+        var modifiers = getSearchModifiers();
+        var ordering = getSortModifier();
 
-	// store modifiers only if non-empty; this avoids e.g. empty search
-	// results resetting checkboxes
-	// BUT -- store empty modifiers if search terms changed (reset)
-	if (!$.isEmptyObject(modifiers) || searchchanged) {
-	    if (searchchanged) {
-		modifiers = {};
-		ordering = '';
-	    }
-	    storeSearchModifiers(SMCOOKIE, modifiers);
-	    
-	    var exclusions = [];
+        // store modifiers only if non-empty; this avoids e.g. empty search
+        // results resetting checkboxes
+        // BUT -- store empty modifiers if search terms changed (reset)
+        if (!$.isEmptyObject(modifiers) || searchchanged) {
+            if (searchchanged) {
+            modifiers = {};
+            ordering = '';
+            }
+            storeSearchModifiers(SMCOOKIE, modifiers);
 
-	    for (var key in modifiers) {
-		var value = modifiers[key];
-		if (!value) {
-		    exclusions.push(key);
-		}
-	    }
+            var exclusions = [];
 
-	    // put exclusion modifiers into query string for back-end use
-	    // encode modifiers with base64 to avoid bugs in qs updates
-	    var ex = window.btoa(JSON.stringify(exclusions));
-	    ex = ex.replace(/\+/g, '-').replace(/\//g, '_').replace(/\=+$/, '');
-	    var newuri = updateQueryStringParameter({'ex': ex, 'q' : searchTerm, 'page' : '1', 'order_by' : ordering});
+            for (var key in modifiers) {
+            var value = modifiers[key];
+            if (!value) {
+                exclusions.push(key);
+            }
+            }
 
-	    window.location.href = newuri;
-	    return false;
-	}
-	
+            // put exclusion modifiers into query string for back-end use
+            // encode modifiers with base64 to avoid bugs in qs updates
+            var ex = window.btoa(JSON.stringify(exclusions));
+            ex = ex.replace(/\+/g, '-').replace(/\//g, '_').replace(/\=+$/, '');
+            var newuri = updateQueryStringParameter({'ex': ex, 'q' : searchTerm, 'page' : '1', 'order_by' : ordering});
+
+            window.location.href = newuri;
+            return false;
+        }
+
         return true;
     }
 }
@@ -93,6 +99,7 @@ function checkSearch() {
    that are either free or that have a free trial.
 */
 function getSearchModifiers() {
+    console.log('getSearchModifiers: begin');
     var modifiers = {};
     $('.searchmod').each(function (k, e) {
 	var modifier = e.getAttribute(SMID);
@@ -108,16 +115,18 @@ function getSearchModifiers() {
    expensive.
 */
 function getSortModifier() {
+    console.log('getSortModifier: begin');
     var sortorder = $('#searchorder').val();
     return sortorder;
 }
 
 /* Retrieve search modifiers from cookie storage */
 function retrieveSearchModifiers(cookieName) {
+    console.log('retrieveSearchModifiers: begin');
     /* Disabling cookied search preferences until they can be remembered
-       per-topic and put in something more appropriate like Local Storage 
+       per-topic and put in something more appropriate like Local Storage
 
-    var stored = $.cookie(cookieName); 
+    var stored = $.cookie(cookieName);
     */
     var stored = null;
 
@@ -135,6 +144,7 @@ function retrieveSearchModifiers(cookieName) {
 /* Store search modifiers in a session cookie, so that checked boxes retain
    state on page reload/navigation. */
 function storeSearchModifiers(cookieName, values) {
+    console.log('storeSearchModifiers: begin');
     var serialized = JSON.stringify(values);
     $.cookie(cookieName, serialized);
 }
@@ -143,6 +153,7 @@ function storeSearchModifiers(cookieName, values) {
 /* Update query string, assigning a new value to each parameter
  * given in kvpairs */
 function updateQueryStringParameter(kvpairs) {
+    console.log('updateQueryStringParameter: begin');
     var uri = URI(window.location.href);
 
     for (var key in kvpairs) {
@@ -156,6 +167,7 @@ function updateQueryStringParameter(kvpairs) {
 
 /* Get a query string parameter by name */
 function getParameterByName(name) {
+    console.log('getParameterByName: begin' + name);
     var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
     return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
 }
@@ -199,52 +211,66 @@ $(document).ready(function () {
 	    // only handle search modifier checkboxes
 	    var searchmod = box.getAttribute(SMID);
 	    var stored_checkvalue = stored[searchmod];
-		
+
 	    // no stored value found -- set checked by default
 	    if (stored_checkvalue == null) {
 		$(box).prop('checked', true);
 	    }
-	    
+
 	    // set box to prior stored value
 	    else {
 		$(box).prop('checked', stored_checkvalue);
 	    }
-	    
+
 	}
     }
-    
+
     // Set up filter-reset click handler to clear search modifier checkboxes
     $("#resetfilters").click(function(event) {
-	$.removeCookie(SMCOOKIE);
-	$('input.searchmod').prop('checked', true);
-	event.preventDefault();
-	$('#searchbutton').click();
+	    $.removeCookie(SMCOOKIE);
+	    $('input.searchmod').prop('checked', true);
+	    event.preventDefault();
+	    $('#bq_searchbutton').click();
     });
 
     // Checkbox click handler to refresh page when search modifiers change
     $(".searchmod").click(function(event) {
-	$('#searchbutton').click();
+        console.log('searchmod click event: before searchbutton click');
+        $('#bq_searchbutton').click();
     });
 
     // if user changes search terms, reset search filters
-    searchval = $("#id_q").val().trim();
+    searchval = $("#bq_query").val().trim();
+    locationval = $("#bq_location").val().trim();
     searchchanged = false;
 
-    $("#id_q").change(function(event) {
-	if (($("#id_q").val().trim() != searchval) && searchval != '') {
-	    searchchanged = true;
-	}
-	else {
-	    searchchanged = false;
-	}
+    $("#bq_query").change(function(event) {
+        if (
+            (($("#bq_query").val().trim() != searchval) && searchval != '')
+        ) {
+            searchchanged = true;
+        }
+        else {
+            searchchanged = false;
+        }
     });
+
+    $("#bq_location").change(function(event) {
+        if (($("#bq_location").val().trim() != searchval) && searchval != '') {
+            searchchanged = true;
+        }
+        else {
+            searchchanged = false;
+        }
+    });
+
 
     // Facet mouseover UI and 'only' selection controls
     $(".facetrow").hover(
 	function () {
 	    $(this).css("background", "aliceblue");
 	    $(this).find("a").show();
-	}, 
+	},
 	function () {
 	    $(this).css("background","");
 	    $(this).find("a").hide();
@@ -252,148 +278,10 @@ $(document).ready(function () {
     );
 
     $(".filteronly").click(function(event) {
-	// have we made check box group read-only? then prevent changes
-	var disabled = $(event.target).parents('dl').children().first().prop('disabled');
-
-	if (disabled != 'disabled') {
 	    // uncheck all filters in group, then check only clicked-row filter
-	    $(this).closest("div").find("input").prop("checked", false);
-	    $(this).closest("dd").find("input").prop("checked", true);
+	    $(this).closest("div.facet").find("input").prop("checked", false);
+	    $(this).closest("div").find("input").prop("checked", true);
 	    event.preventDefault();
-	    $("#searchbutton").click();
-	}
+	    $("#bq_searchbutton").click();
     });
-
-    // Hide excess rows under Category and Provider groups, with "more" link
-    // to reveal them when clicked
-    var initialRows = 3;
-    var hideTargets = ['#group_category dd', '#group_provider dd'];
-    var moreLink = '<dd><a class="moreLink" href="#">more &raquo;</a></dd>';
-
-    for (var k=0; k < hideTargets.length; k += 1) {
-	var categories = $(hideTargets[k]);
-	if (categories.length > initialRows) {
-	    // add a 'more' link at the end of category
-	    var inserted = $(moreLink).insertAfter(categories.last());
-
-	    inserted.click(function(event) {
-		// reveal previously-hidden category rows on 'more' link click
-		$(event.target).parent().parent().find('dd').show();
-		// hide 'more' link after revealing extras
-		$(event.target).hide();
-		event.preventDefault();
-	    });
-
-	    // hide the excess rows on initial page view
-	    categories.slice(initialRows, 1000).hide();
-	}
-    }
-
-    // Make search box read-only if user is on a pre-filtered content page or faceting
-    // Also hide search area if user is on a pre-filtered content page
-    var slugs = getFacets();
-    if (slugs.length) {
-	$('.input-group').hide();
-	
-	var restrictions = {'category_exact' : 'dl :contains(Category)',
-			    'school_exact' : 'dl :contains(Provider)',
-			    'price_bucket_exact' : 'dl :contains(Pricing)',
-			    'certificate_exact' : 'dl :contains(Certificate)'};
-
-	for (var j = 0; j < slugs.length; j += 1) {
-	    slug = slugs[j];
-
-	    // find which slugtype (if any) matches current slug, restrict its search group
-	    for (var slugtype in restrictions) {
-		if (slug.indexOf(slugtype) > -1) {
-		    var boxes = $(restrictions[slugtype]).parent().find('input');
-		    boxes.prop('checked', false);
-		    for (var k=0; k < boxes.length; k += 1) {
-			var box = $(boxes[k]);
-			var bdata = box.data()['searchmod'];
-			if (bdata == slug) {
-			    box.prop('checked', true);
-			}
-		    }
-		    boxes.prop('disabled', 'disabled');
-		    boxes.first().parents('dl').children().first().prop('disabled', 'disabled');
-		}
-	    }
-	}
-    }
-
-    // set up search ordering controls
-    var default_order = 'relevance';
-    var orderings = Array(Array("Sort: Relevance", "relevance"),
-              Array("Sort: Price low to high", "price_low"),
-              Array("Sort: Price high to low", "price_high"),
-			  Array("Sort: Popularity", "popularity"));
-
-    // don't provide price sorting when price filter is stuck on Free
-
-    var price_rejects = Array(Array('price_bucket_exact:Free'),
-			      Array('price_bucket_exact:Free Trial'),
-			      Array('price_bucket_exact:Free', 
-				    'price_bucket_exact:Free Trial')
-			     );
-    var prices = $("dl dt:contains(Pricing)").parent().find('input:checked');
-    var hide_price_sort = false;
-    if (prices.length == 1 && prices.first().data('searchmod') == 'price_bucket_exact:Free') {
-	hide_price_sort = true;
-    }
-
-    if (hide_price_sort) {
-	var newOrderings = Array();
-	for (j=0; j < orderings.length; j += 1) {
-	    if (orderings[j][0].indexOf("Price") == -1) {
-		newOrderings.push(orderings[j]);
-	    }
-	}
-
-	orderings = newOrderings;
-    }
-
-    // don't provide Relevance sorting if there is no explicit search term
-    var query = getParameterByName('q');
-    if (!query || !query.trim()) {
-	var default_order = 'popularity';
-	var newOrderings = Array();
-	for (j=0; j < orderings.length; j += 1) {
-	    if (orderings[j][0].indexOf("Relevance") == -1) {
-		newOrderings.push(orderings[j]);
-	    }
-	}
-
-	orderings = newOrderings;
-    }
-    
-
-    for(j=0; j < orderings.length; j += 1)
-    {
-	var key = orderings[j][0];
-	var value = orderings[j][1];
-	var rhtml = ['<option value="', value, '">', key, '</option>'].join('');
-	var opt = $(rhtml);
-	opt.appendTo("#searchorder");
-    }
-
-    // set stored search order
-    var stored_order = getParameterByName('order_by');
-    if (stored_order) {
-	$("#searchorder").val(stored_order);
-    }
-    else {
-	$("#searchorder").val(default_order);
-    }
-
-    // hide search options if there is only one search option
-    if (orderings.length == 1) {
-	$("#searchblock").hide();
-    }
-
-    // Search order change handler to refresh page when search modifiers change
-    $("#searchorder").change(function(event) {
-	$('#searchbutton').click();
-    });
-
 });
