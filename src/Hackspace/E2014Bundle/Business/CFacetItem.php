5448107777c849d2df788a61eb63a6b090ebd910
Carlos Salvatierra <cslucano@gmail.com>
2014-10-10 00:42:02 -0500

    added fields to CFacetItem

--- src/Hackspace/E2014Bundle/Business/CFacetItem.php
+++ src/Hackspace/E2014Bundle/Business/CFacetItem.php
@@ -1,1 +1,49 @@
 <?php
+
+namespace Hackspace\E2014Bundle\Business;
+
+class CFacetItem
+{
+    protected $term;
+    protected $count;
+
+    /**
+     * @return string
+     */
+    public function getTerm()
+    {
+        return $this->term;
+    }
+
+    /**
+     * @param string $term
+     *
+     * @return $this
+     */
+    public function setTerm($term)
+    {
+        $this->term = $term;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCount()
+    {
+        return $this->count;
+    }
+
+    /**
+     * @param string $count
+     *
+     * @return $this
+     */
+    public function setCount($count)
+    {
+        $this->count = $count;
+
+        return $this;
+    }
+}
